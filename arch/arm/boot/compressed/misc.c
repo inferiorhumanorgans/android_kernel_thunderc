e8e9e8b74d40bbad774978334ff0cb37b70864a4
Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
2010-02-04 10:23:58 +0100

    ARM: remove bit-rotten STANDALONE_DEBUG for decompressor
    I tried to get this running to debug the regression introduced by
    e7db7b4 without success.  But this has several problems that make it
    hard to fix:
    
      - lib/decompress_inflate.c includes in-kernel headers that make
        it difficult to compile for user space.
      - the binary formats changed both in kernel and user space and
        at least for the kernel side there isn't only a single
        variant.
    
    Signed-off-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
    Acked-by: Nicolas Pitre <nicolas.pitre@linaro.org>
    
    Conflicts:
    
    	arch/arm/boot/compressed/misc.c

--- arch/arm/boot/compressed/misc.c
+++ arch/arm/boot/compressed/misc.c
@@ -23,10 +23,6 @@ unsigned int __machine_arch_type;
 #include <linux/stddef.h>	/* for NULL */
 #include <asm/string.h>
 
-#ifdef STANDALONE_DEBUG
-#define putstr printf
-#else
-
 static void putstr(const char *ptr);
 
 #include <mach/uncompress.h>
@@ -98,8 +94,6 @@ static void putstr(const char *ptr)
 
 	flush();
 }
-
-#endif
 
 #define __ptr_t void *
 
@@ -314,8 +308,6 @@ static void error(char *x)
 	while(1);	/* Halt */
 }
 
-#ifndef STANDALONE_DEBUG
-
 ulg
 decompress_kernel(ulg output_start, ulg free_mem_ptr_p, ulg free_mem_ptr_end_p,
 		  int arch_id)
@@ -333,19 +325,3 @@ decompress_kernel(ulg output_start, ulg free_mem_ptr_p, ulg free_mem_ptr_end_p,
 	putstr(" done, booting the kernel.\n");
 	return output_ptr;
 }
-#else
-
-char output_buffer[1500*1024];
-
-int main()
-{
-	output_data = output_buffer;
-
-	makecrc();
-	putstr("Uncompressing Linux...");
-	gunzip();
-	putstr("done.\n");
-	return 0;
-}
-#endif
-	
