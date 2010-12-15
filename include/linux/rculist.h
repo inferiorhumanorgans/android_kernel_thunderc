e41e1e3259d1818f5640c2668ff0ce3c5afada7a
Mariusz Kozlowski <mk@lab.zgora.pl>
2010-12-15 23:11:12 +0100

    rculist: fix borked __list_for_each_rcu() macro
    This restores parentheses blance.
    
    Signed-off-by: Mariusz Kozlowski <mk@lab.zgora.pl>
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
    
    Conflicts:
    
    	include/linux/rculist.h

--- include/linux/rculist.h
+++ include/linux/rculist.h
@@ -242,7 +242,7 @@ static inline void list_splice_init_rcu(struct list_head *list,
 #define __list_for_each_rcu(pos, head) \
 	for (pos = rcu_dereference_raw((head)->next); \
 		pos != (head); \
-		pos = rcu_dereference_raw(pos->next))
+		pos = rcu_dereference_raw(list_next_rcu(pos)))
 
 /**
  * list_for_each_entry_rcu	-	iterate over rcu list of given type
