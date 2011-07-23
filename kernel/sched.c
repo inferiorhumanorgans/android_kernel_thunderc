e1ec3bfe498c46c1a4d3f9fa129af1cbc1f710cb
Alex Zepeda <alex@inferiorhumanorgans.com>
2011-07-23 00:13:31 -0700

    Kernel 64
    - Revert to 'ondemand' governor for battery life.
    - Include 'smartass' governor for testing.

--- kernel/sched.c
+++ kernel/sched.c
@@ -10970,5 +10970,6 @@ void synchronize_sched_expedited(void)
 		synchronize_sched();
 }
 EXPORT_SYMBOL_GPL(synchronize_sched_expedited);
+EXPORT_SYMBOL_GPL(nr_running);
 
 #endif /* #else #ifndef CONFIG_SMP */
